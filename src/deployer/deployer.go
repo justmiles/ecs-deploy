e1d6da1bb630a6c95950a0551edffb999add6592
Chris Pruitt <chris.pruitt15@gmail.com>
2023-01-29 00:27:42 -0500

    refresh-secrets, dry-run during ship

--- src/deployer/deployer.go
+++ src/deployer/deployer.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -12,6 +13,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/aws/aws-sdk-go/service/ssm"
+
+	"github.com/mitchellh/copystructure"
 )
 
 var (
@@ -28,9 +31,11 @@ var (
 func PerformDeployment(depOpts DeploymentOptions) (s string, err error) {
 	var deploymentResults DeploymentResults
 	// Set the desired application version
-	err = setDesiredVersion(depOpts)
-	if err != nil {
-		return s, err
+	if !depOpts.DryRun {
+		err = setDesiredVersion(depOpts)
+		if err != nil {
+			return s, err
+		}
 	}
 
 	var svc *ecs.ECS
@@ -68,18 +73,32 @@ func PerformDeployment(depOpts DeploymentOptions) (s string, err error) {
 		return s, err
 	}
 
+	// Deep copy to preserve original container definitions for diff
+	copyContainerDefs, err := copystructure.Copy(dtdo.TaskDefinition.ContainerDefinitions)
+	if err != nil {
+		fmt.Printf("Error performing deep copy of container definitions: %v", err)
+		os.Exit(1)
+	}
+	desiredContainerDefinitions, ok := copyContainerDefs.([]*ecs.ContainerDefinition)
+	if !ok {
+		fmt.Printf("Error converting interface to ecs.ContainerDefinition: %v", err)
+		os.Exit(1)
+	}
+
 	// Update only first container definition
-	repoAndVersion := strings.Split(*dtdo.TaskDefinition.ContainerDefinitions[0].Image, ":")
+	repoAndVersion := strings.Split(*desiredContainerDefinitions[0].Image, ":")
 	if len(repoAndVersion) == 1 {
 		repoAndVersion = append(repoAndVersion, depOpts.Version)
 	} else {
 		repoAndVersion[1] = depOpts.Version
 	}
-	*dtdo.TaskDefinition.ContainerDefinitions[0].Image = strings.Join(repoAndVersion, ":")
+
+	// Update only the first contianer image version - ignore sidecar containers assuming they are defined second, third, and so on.
+	*desiredContainerDefinitions[0].Image = strings.Join(repoAndVersion, ":")
 
 	// Register new task definition
 	rtdi := &ecs.RegisterTaskDefinitionInput{
-		ContainerDefinitions:    dtdo.TaskDefinition.ContainerDefinitions,
+		ContainerDefinitions:    desiredContainerDefinitions,
 		Cpu:                     dtdo.TaskDefinition.Cpu,
 		ExecutionRoleArn:        dtdo.TaskDefinition.ExecutionRoleArn,
 		Family:                  dtdo.TaskDefinition.Family,
@@ -90,24 +109,106 @@ func PerformDeployment(depOpts DeploymentOptions) (s string, err error) {
 		Volumes:                 dtdo.TaskDefinition.Volumes,
 		RequiresCompatibilities: dtdo.TaskDefinition.RequiresCompatibilities,
 	}
+
+	if depOpts.RefreshSecrets {
+		var ssmClient *ssm.SSM
+		if depOpts.Role != "" {
+			creds := stscreds.NewCredentials(sess, depOpts.Role)
+			ssmClient = ssm.New(sess, &aws.Config{Credentials: creds})
+		} else {
+			ssmClient = ssm.New(sess)
+		}
+
+		pageNum := 0
+		containerSecrets := []*ecs.Secret{}
+		err := ssmClient.GetParametersByPathPages(&ssm.GetParametersByPathInput{
+			Path: aws.String(depOpts.SecretsPrefix),
+		},
+			func(page *ssm.GetParametersByPathOutput, lastPage bool) bool {
+				pageNum++
+				for _, v := range page.Parameters {
+
+					ss := strings.Split(*v.Name, "/")
+					s := ss[len(ss)-1]
+
+					secret := &ecs.Secret{
+						Name:      aws.String(s),
+						ValueFrom: v.ARN,
+					}
+					containerSecrets = append(containerSecrets, secret)
+				}
+				return pageNum <= 100
+			})
+
+		if err != nil {
+			fmt.Printf("Error refreshing ssm params: %v", err)
+			os.Exit(1)
+		}
+
+		desiredContainerDefinitions[0].Secrets = containerSecrets
+
+	}
+
+	// Diff task image version
+	diff := NewDiff("container", *dtdo.TaskDefinition.ContainerDefinitions[0].Name)
+	for index, currentContainerDef := range dtdo.TaskDefinition.ContainerDefinitions {
+		diff.AddChange("image", *currentContainerDef.Image, *desiredContainerDefinitions[index].Image)
+
+		// Diff task secrets
+		for _, x := range currentContainerDef.Secrets {
+			found := false
+			for _, y := range desiredContainerDefinitions[index].Secrets {
+				// TODO diff secrets
+				if *x.Name == *y.Name {
+					diff.AddChange(*x.Name, *x.ValueFrom, *y.ValueFrom)
+					found = true
+				}
+			}
+
+			if !found {
+				diff.AddChange(*x.Name, *x.ValueFrom, "")
+			}
+		}
+
+		for _, y := range desiredContainerDefinitions[index].Secrets {
+			found := false
+			for _, x := range currentContainerDef.Secrets {
+				if *x.Name == *y.Name {
+					found = true
+				}
+			}
+			if !found {
+				diff.AddChange(*y.Name, "", *y.ValueFrom)
+			}
+		}
+
+		if len(diff.changes) > 0 {
+			fmt.Println(diff)
+		}
+	}
+
+	if depOpts.DryRun {
+		os.Exit(0)
+	}
+
 	rtdo, err := svc.RegisterTaskDefinition(rtdi)
 	if err != nil {
 		return s, err
 	}
 	// Update the service with the new task definition
 	usi := &ecs.UpdateServiceInput{
-		Cluster:                       dso.Services[0].ClusterArn,
-		DeploymentConfiguration:       dso.Services[0].DeploymentConfiguration,
-		DesiredCount:                  dso.Services[0].DesiredCount,
-		ForceNewDeployment:            aws.Bool(true),
-		NetworkConfiguration:          dso.Services[0].NetworkConfiguration,
-		PlatformVersion:               dso.Services[0].PlatformVersion,
-		Service:                       dso.Services[0].ServiceArn,
-		TaskDefinition:                rtdo.TaskDefinition.TaskDefinitionArn,
+		Cluster:                 dso.Services[0].ClusterArn,
+		DeploymentConfiguration: dso.Services[0].DeploymentConfiguration,
+		DesiredCount:            dso.Services[0].DesiredCount,
+		ForceNewDeployment:      aws.Bool(true),
+		NetworkConfiguration:    dso.Services[0].NetworkConfiguration,
+		PlatformVersion:         dso.Services[0].PlatformVersion,
+		Service:                 dso.Services[0].ServiceArn,
+		TaskDefinition:          rtdo.TaskDefinition.TaskDefinitionArn,
 	}
 	// If HealthCheckGracePeriodSeconds == 0 (Default), assume that the previous definition did not include a health check.
-	if  *dso.Services[0].HealthCheckGracePeriodSeconds != 0 {
-    usi.HealthCheckGracePeriodSeconds = dso.Services[0].HealthCheckGracePeriodSeconds
+	if *dso.Services[0].HealthCheckGracePeriodSeconds != 0 {
+		usi.HealthCheckGracePeriodSeconds = dso.Services[0].HealthCheckGracePeriodSeconds
 	}
 
 	uso, err := svc.UpdateService(usi)
